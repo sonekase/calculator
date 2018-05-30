9f491c8fb9db72055d8992f8e6398bea3b8bca69
Anousone Kaseumsouk and Lan Dam <anousone@>
2018-05-30 16:09:35 -0700

    Split each function into its own column.

--- js/scripts.js
+++ js/scripts.js
@@ -21,27 +21,27 @@ $(document).ready(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#output1").text(result);
   });
   $("form#subtract").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#subtract1").val());
     var number2 = parseInt($("#subtract2").val());
     var result = subtract(number1, number2);
-    $("#output").text(result);
+    $("#output2").text(result);
   });
   $("form#multiply").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#multiply1").val());
     var number2 = parseInt($("#multiply2").val());
     var result = multiply(number1, number2);
-    $("#output").text(result);
+    $("#output3").text(result);
   });
   $("form#divide").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#divide1").val());
     var number2 = parseInt($("#divide2").val());
     var result = divide(number1, number2);
-    $("#output").text(result);
+    $("#output4").text(result);
   });
 });
